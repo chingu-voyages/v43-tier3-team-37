68de8e94d009c814ca95f721e0fd763ca18fca93
mariam.mahareeq <mariam.mahareeq@transcend.ps>
2023-03-25 02:24:54 +0300

    create hero and menu section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import Theme from "./Theme";
 import Hero from "./components/Hero/Hero";
+import Home from "./pages/Home/Home";
 import "./App.css";
 function App() {
   return (
     <Theme>
       <div className="App">
-       <Hero hero='About Us'></Hero>
+       {/* <Hero hero='About Us'></Hero> */}
+       <Home/>
       </div>
     </Theme>
   );
--- src/styles/ThemeButton.styled.js
+++ src/styles/ThemeButton.styled.js
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const ThemeButton = styled.button`
-  font-size: 18px;
+  font-size: 15px;
   font-family: ${({ theme }) => theme.fonts[2]};
   border: 1px solid ${({ theme }) => theme.colors.softorange};
   background: ${({theme})=> theme.colors.white};
@@ -20,4 +20,8 @@ export const ThemeFillButton = styled.button`
   height: 40px;
   cursor: pointer;
  
+`;
+export const Button = styled(ThemeButton)`
+  background: initial;
+  color: ${(props) => props.color ? props.color :props.theme.colors.white};
 `;
