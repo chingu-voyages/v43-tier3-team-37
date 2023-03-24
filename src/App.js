62c5cd32ae236962fbe0324d9c078d21c1e22f85
Morgan <morganjbare@gmail.com>
2023-03-24 11:04:12 +0100

    Set up home page, components/pages/assets folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from "./logo.svg";
 import Theme from "./Theme";
 import "./App.css";
+import Home from "./pages/Home";
 function App() {
   return (
     <Theme>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Home/>
       </div>
     </Theme>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Services } from '../components'
+
+function Home() {
+  return (
+    <>
+    <div>Home</div>
+    <Services/>
+    </>
+  )
+}
+
+export default Home
