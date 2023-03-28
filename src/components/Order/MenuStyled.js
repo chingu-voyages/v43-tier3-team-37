a4153d13cc8e17decaf27342299f2bb16e5bfa78
Galal Mohamed <galalmgy@gmail.com>
2023-03-28 02:38:48 +0200

    updating images

--- src/components/Order/MenuStyled.js
+++ src/components/Order/MenuStyled.js
@@ -5,18 +5,24 @@ export const MenuContainer = styled.div`
   grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
   grid-gap: 20px;
   justify-items: center;
-  align-items: center;
+  align-items: stretch;
+  margin-right: 50px;
+  margin-left: 50px;
 `;
 
 export const MenuItem = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: space-between;
+  height: 400px; /* set a fixed height for the menu item container */
 `;
 
 export const Image = styled.img`
   width: 100%;
-  height: auto;
+  height: 320px;
+  aspect-ratio: 1/1; /* set a fixed aspect ratio for the image */
+  object-fit: cover; /* scale the image to cover the entire container */
   margin-bottom: 10px;
 `;
 
--- src/components/Shared/Dishes.js
+++ src/components/Shared/Dishes.js
@@ -1,131 +1,122 @@
+const importAll = (r) => r.keys().map(r);
+
+const images = importAll(
+  require.context("../../assets/images/Menu", false, /\.(png|jpe?g|svg)$/)
+);
+
 const dishes = [
   {
     id: 1,
     dishType: "Main Dishes",
     name: "Spaghetti Carbonara",
     price: "$12.99",
-    image:
-      "https://www.pexels.com/photo/pasta-with-shrimp-on-white-ceramic-plate-4518839/300x200",
+    image: `${images.find((img) => img.includes("Spaghetti"))}`,
   },
   {
     id: 2,
     dishType: "Main Dishes",
     name: "Margherita Pizza",
     price: "$9.99",
-    image:
-      "https://www.pexels.com/photo/top-view-photo-of-baked-pizza-2147491/300x200",
+    image: `${images.find((img) => img.includes("MargheritaPizza"))}`,
   },
+
   {
     id: 3,
     dishType: "Main Dishes",
-    name: "Caesar Salad",
-    price: "$8.99",
-    image: "https://www.pexels.com/photo/salad-on-a-plate-2097090/300x200",
+    name: "Chicken Alfredo",
+    price: "$14.99",
+    image: `${images.find((img) => img.includes("ChickenAlfredo"))}`,
   },
   {
     id: 4,
     dishType: "Main Dishes",
-    name: "Chicken Alfredo",
-    price: "$14.99",
-    image:
-      "https://www.pexels.com/photo/delicious-traditional-italian-fettuccine-alfredo-pasta-11220209/300x200",
+    name: "Beef Tacos",
+    price: "$10.99",
+    image: `${images.find((img) => img.includes("BeefTacos"))}`,
   },
   {
     id: 5,
-    dishType: "Main Dishes",
-    name: "Beef Tacos",
-    price: "$10.99",
-    image:
-      "https://www.pexels.com/photo/tacos-in-close-up-photography-7904958/300x200",
+    dishType: "Starters",
+    name: "Bruschetta",
+    price: "$8.99",
+    image: `${images.find((img) => img.includes("Bruschetta"))}`,
   },
   {
     id: 6,
     dishType: "Starters",
-    name: "Bruschetta",
-    price: "$8.99",
-    image:
-      "https://images.unsplash.com/photo-1602521709105-5fa47ec5c3d5?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJ1c2NoZXR0YXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Caesar Salad",
+    price: "$7.99",
+    image: `${images.find((img) => img.includes("CaesarSalad"))}`,
   },
   {
     id: 7,
     dishType: "Starters",
-    name: "Caesar Salad",
-    price: "$7.99",
-    image:
-      "https://images.unsplash.com/photo-1522542550221-31fd19575a2c?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2Flc2FyJTIwc2FsYWR8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Caprese Salad",
+    price: "$9.99",
+    image: `${images.find((img) => img.includes("CapreseSalad"))}`,
   },
   {
     id: 8,
     dishType: "Starters",
-    name: "Caprese Salad",
-    price: "$9.99",
-    image:
-      "https://images.unsplash.com/photo-1592327064819-3b3a2b2f1b64?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FwcmVzZSUyMHNhbGFkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Garlic Bread",
+    price: "$5.99",
+    image: `${images.find((img) => img.includes("GarlicBread"))}`,
   },
   {
     id: 9,
     dishType: "Starters",
-    name: "Garlic Bread",
-    price: "$5.99",
-    image:
-      "https://images.unsplash.com/photo-1620233065226-1527f28fb1ed?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGdhcmxpYyUyMGJyZWFkfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    name: "Mozzarella Sticks",
+    price: "$6.99",
+    image: `${images.find((img) => img.includes("MozzarellaSticks"))}`,
   },
   {
     id: 10,
-    dishType: "Starters",
-    name: "Mozzarella Sticks",
+    dishType: "Desserts",
+    name: "Tiramisu",
     price: "$6.99",
-    image:
-      "https://images.unsplash.com/photo-1612408023917-c55a240e8f20?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fG1vemVsYXJyZWElMjBzdGlja3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    image: `${images.find((img) => img.includes("Tiramisu"))}`,
   },
   {
     id: 11,
-    name: "Tiramisu",
-    price: "$6.99",
-    image:
-      "https://images.unsplash.com/photo-1525295847127-b6fa481a0e8f?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fHRpcmFtaXN1JTIwYnJlYWQlMjBkZXNzZXJ0c3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    dishType: "Desserts",
+    name: "Chocolate Cake",
+    price: "$7.99",
+    image: `${images.find((img) => img.includes("ChocolateCake"))}`,
   },
   {
     id: 12,
-    name: "Chocolate Cake",
-    price: "$7.99",
-    image:
-      "https://images.unsplash.com/photo-1605170111452-86205c34fa85?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzR8fGNoYXRsZW5nZSUyMGNha2VzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    dishType: "Desserts",
+    name: "Fruit Salad",
+    price: "$5.99",
+    image: `${images.find((img) => img.includes("FruitSalad"))}`,
   },
   {
     id: 13,
-    name: "Fruit Salad",
-    price: "$5.99",
-    image:
-      "https://images.unsplash.com/photo-1505761671935-60b3a7427bad?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJ1aXRzJTIwc2FsYWQlMjBjb2xvclxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    dishType: "Desserts",
+    name: "Ice Cream Sundae",
+    price: "$4.99",
+    image: `${images.find((img) => img.includes("IceCreamSundae"))}`,
   },
   {
     id: 14,
-    name: "Ice Cream Sundae",
-    price: "$4.99",
-    image:
-      "https://images.unsplash.com/photo-1567706070544-4c8e1d8444c4?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlJTIwY3JlYWx8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    dishType: "Drinks",
+    name: "Iced Tea",
+    price: "$2.99",
+    image: `${images.find((img) => img.includes("IcedTea"))}`,
   },
   {
     id: 15,
-    name: "Iced Tea",
+    dishType: "Drinks",
+    name: "Lemonade",
     price: "$2.99",
-    image:
-      "https://images.unsplash.com/photo-1584974497616-8c6b22915c05?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8aWNlZCUyMHRlYXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    image: `${images.find((img) => img.includes("Lemonade"))}`,
   },
   {
     id: 16,
-    name: "Lemonade",
-    price: "$2.99",
-    image:
-      "https://images.unsplash.com/photo-1584974600878-ee5c5f0a0e5e?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bGVtb25hZGUlMjBjYWtlc3xlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
-  },
-  {
-    id: 17,
+    dishType: "Drinks",
     name: "Orange Juice",
     price: "$3.99",
-    image:
-      "https://images.unsplash.com/photo-1519681393784-d120267933ba?ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8b3JhbmdlJTIwanVpY2UlMjBjaGVjZWxsfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    image: `${images.find((img) => img.includes("OrangeJuice"))}`,
   },
 ];
 
